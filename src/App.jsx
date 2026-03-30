0ea4542cd8b944a52127546125a17d8f3eaa049c
RCNT04 <siriwanit@gmail.com>
2026-03-30 15:15:18 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
   const messagesEndRef = useRef(null);
   const fileInputRef = useRef(null);
   
-const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
+const apiKey = const apiKey = "AIzaSyC1EOfDaaeoH_oRNtQGoXxtmALqF-Vm-mQ";
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
